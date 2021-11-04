20b7d85199fa4a7bd0deca82f3fe9481675233a7
pablomigon <80349635+pablomigon@users.noreply.github.com>
2021-11-04 16:39:23 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ switch ($action)
 	case 'lista-categorias':
 		include __DIR__.'/category_list_resource.php';
 		break;
+	
+	case 'products':
+		include __DIR__.'/product_resource.php';
+		break;
 	default:
 		include __DIR__.'/home_resource.php';
 		break;
